9bcbd7f91c076d2d98c03f75706eb2521c446560
unknown <Emes2@EMES2.emes.local>
2023-11-24 12:08:24 +0100

    Implémentation d'un signup basique qui ajoute le username et le password dans un state

--- weather-app/src/components/Signup.js
+++ weather-app/src/components/Signup.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link} from 'react-router-dom';
 
-const Signup = () => {
+const Signup = (props) => {
+
+    const {profil,addProfil} = props
+
+
+    const [inUser,setInUser] = useState(
+      {
+        username : '',
+        password : '',
+      }
+    )
+
+    const handleInputChange = (e) => {
+      const {name,value} = e.target
+      setInUser(
+        (prevUser) => ({...prevUser,[name]:value})
+      )
+    } 
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      addProfil(inUser)
+      setInUser({
+        username : '',
+        password: ''
+      })
+    }
+
     return(
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Create your account</h2>
         </div>
       
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form class="space-y-6" action="#" method="POST">
+          <form class="space-y-6" onSubmit={handleSubmit}>
             <div>
-              <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+              <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
               <div class="mt-2">
-                <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <input value={inUser.username} onChange={handleInputChange}  id="username" name="username" type="username" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
               </div>
             </div>
       
@@ -22,18 +50,9 @@ const Signup = () => {
                 <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
               </div>
               <div class="mt-2">
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <input value={inUser.password} onChange={handleInputChange} id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
               </div>
             </div>
-
-            <div>
-                <div class="flex items-center justify-between">
-                  <label for="repassword" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
-                </div>
-                <div class="mt-2">
-                  <input id="repassword" name="repassword" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                </div>
-              </div>
       
             <div>
               <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
@@ -9,12 +8,5 @@ import Dashboard from './components/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-        <Routes>
-            <Route path="" element={<App />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-    </Router>
+    <App/>
 );
